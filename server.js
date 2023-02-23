1efab8cf6d47f3a63aeb38d411e878cc31e55ad0
Leonardo Fonseca <leonardofs@dcc.ufrj.com.br>
2023-02-22 21:06:37 -0300

    subindo template

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('./src/config/dotenv')();
+//require('./src/config/sequelize');
+
+const app = express();
+const port = process.env.PORT;
+//const cors = require('cors');
+//const routes = require('./src/routes/routes');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//app.use(routes);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`${process.env.APP_NAME} app listening at http://localhost:${port}`);
+});
+    
